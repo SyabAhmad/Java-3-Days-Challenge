e9f0ac6134166262ffe7f425b9e5147935af5aa8
SyabAhmad <syabblogger@gmail.com>
2023-02-21 16:12:04 +0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[]args)
+    {
+        //// this is hello Developer Program in Java
+        System.out.println("Hello Developer");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,53 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Java 3 Days Journey ");
+        System.out.println("We will cover all these topics 1 by 1");
+        System.out.println("1: Hello World Program");
+        System.out.println("1: Variables");
+        System.out.println("1: DataTypes");
+        System.out.println("1: Operators");
+        System.out.println("1: if else");
+        System.out.println("1: switch");
+        System.out.println("1: for loop");
+        System.out.println("1: while loop");
+        System.out.println("1: do while");
+        System.out.println("1: Functions");
+        System.out.println("1: classes");
+        System.out.println("1: inheritance");
+        System.out.println("1: polymorphic inheritance");
+        System.out.println("1: abstraction");
+        System.out.println("1: interfaces");
+        System.out.println("1: Arrays");
+        System.out.println("1: List");
+        System.out.println("1: LinkedList");
+        System.out.println("1: stack");
+        System.out.println("1: Searching");
+        System.out.println("1: Traversing");
+        System.out.println("1: Sorting");
+        System.out.println("1: Merging");
+        System.out.println("1: insertion");
+        System.out.println("1: deletion");
+        System.out.println("1: new keyword");
+        System.out.println("1: this keyword");
+        System.out.println("1: constructor");
+        System.out.println("1: return keyword");
+        System.out.println("1: default keyword");
+        System.out.println("1: break keyword");
+        System.out.println("1: continue keyword");
+        System.out.println("1: ");
+        System.out.println("1: ");
+        System.out.println("1: ");
+        System.out.println("1: ");
+        System.out.println("1: ");
+        System.out.println("1: ");
+        System.out.println("1: ");
+        System.out.println("1: ");
+        System.out.println("1: ");
+        System.out.println("1: ");
+        System.out.println("1: ");
+        System.out.println("1: ");
+        System.out.println("1: ");
+        System.out.println("1: ");
+    }
+}
